97d80442a2c5065deaf3a1fb71af03d07f0fd5e0
Shargorodskyi <gsmkibernetik@gmail.com>
2021-11-14 20:07:09 +0200

    commit with append urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     path('admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
+
+
+with open("poem.txt", "r", encoding = "utf-8") as file:
+    data = file.read(1)
+    print(data)
+    data = file.read()
+    print(data)
